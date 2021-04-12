873211d10effc6cda409ff51b8271c3b0c95e5e7
Sam Boesjes <534268@student.glu.nl>
2021-04-12 23:10:34 +0200

    Faqs Done

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
-import React from 'react';
-import { FooterContainer } from './containers/footer';
-import { JumbotronContainer } from './containers/jumotron';
+import React from "react";
+import { FaqsContainer } from "./containers/faws";
+import { FooterContainer } from "./containers/footer";
+import { JumbotronContainer } from "./containers/jumotron";
 
 export default function App() {
   return (
     <>
       <JumbotronContainer />
+      <FaqsContainer />
       <FooterContainer />
     </>
   );

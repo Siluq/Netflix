46a68915c9e8d0ea54eb496b1ead4a681cf6c218
Sam Boesjes <534268@student.glu.nl>
2021-04-19 13:14:24 +0200

    Router Routes Done

--- src/App.js
+++ src/App.js
@@ -6,9 +6,17 @@ import * as ROUTES from "./constants/routes";
 export default function App() {
   return (
     <Router>
-      <Route exact path={ROUTES.HOME}></Route>
-      <Route path="/user">
-        <p>heyy i am a user</p>
+      <Route exact path="/browse">
+        <Browse />
+      </Route>
+      <Route exact path="/signin">
+        <Signin />
+      </Route>
+      <Route exact path="/signup">
+        <Signup />
+      </Route>
+      <Route exact path={ROUTES.HOME}>
+        <Home />
       </Route>
     </Router>
   );
